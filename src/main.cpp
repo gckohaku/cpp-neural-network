a3cdf3e31f95f269d297e86c986a30b0e48b984d
gckohaku <cyanhairneko@gmail.com>
2026-05-26 16:09:18 +0900

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
+#include <array>
+#include <cblas.h>
 #include <iostream>
-#include <eigen3/Eigen/Dense>
+#include <vector>
 
 int main() {
-    Eigen::Matrix3d m = Eigen::Matrix3d::Random();
+    // a row, c row value
+    int m = 2;
+    // a column, b row value
+    int k = 3;
+    // b column, c column value
+    int n = 2;
 
-    std::cout << m << std::endl;
+    float alpha = 1.0;
+    float beta = 0.0;
+
+    std::vector<float> a = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+    std::vector<float> b = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+
+    std::vector<float> c(m * n, 0.0);
+
+    // call cblas_sgemm
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, a.data(), k, b.data(), n, beta, c.data(), n);
+
+    // output result
+    std::cout << "Result matrix c:" << std::endl;
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            std::cout << c[i * n + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
