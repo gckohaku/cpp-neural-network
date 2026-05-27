a3ff5b6bb090680d8309bc31af39bd1c00388c9a
gckohaku <cyanhairneko@gmail.com>
2026-05-27 16:27:04 +0900

    third commit

--- src/matrices/concepts/MatrixConcepts.hpp
+++ src/matrices/concepts/MatrixConcepts.hpp
@@ -1,1 +1,19 @@
 #include <concepts>
+
+template <typename T>
+concept MatrixElement = (std::floating_point<T> || std::integral<T>);
+
+template <typename M, typename K>
+concept MatrixConcept = MatrixElement<K> &&
+                        // 行列同士の演算
+                        requires(M a, M b) {
+                          { a + b } -> std::same_as<M>;
+                          { a - b } -> std::same_as<M>;
+                          { a * b } -> std::same_as<M>;
+                          { a.dot(b) } -> std::same_as<M>;
+                        } &&
+                        // スカラーと行列の演算
+                        requires(M m, K k) {
+                          { k * m } -> std::same_as<M>;
+                          { m * k } -> std::same_as<M>;
+                        };
