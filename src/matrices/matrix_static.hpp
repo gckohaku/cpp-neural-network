a226588cce50ab1c8fa790306d1095d623171d12
gckohaku <51472484+gckohaku@users.noreply.github.com>
2026-06-16 07:11:48 +0000

    edit MatrixStatic

--- src/matrices/matrix_static.hpp
+++ src/matrices/matrix_static.hpp
@@ -10,14 +10,21 @@ class MatrixStatic {
 private:
     size_t _rowSize = Row;
     size_t _columnSize = Col;
-    std::array<K, Row * Col> elements;
+    std::array<K, Row * Col> _elements;
 
 public:
     MatrixStatic(size_t rowSize, size_t columnSize);
+    MatrixStatic(size_t rowSize, size_t columnSize, std::array<K, Row * Col> elements);
+
+    
 };
 
 template <typename K, size_t Row, size_t Col>
 MatrixStatic<K, Row, Col>::MatrixStatic(size_t rowSize, size_t columnSize)
     : _rowSize(rowSize), _columnSize(columnSize) {}
+
+template <typename K, size_t Row, size_t Col>
+MatrixStatic<K, Row, Col>::MatrixStatic(size_t rowSize, size_t columnSize, std::array<K, Row * Col> elements)
+    : _rowSize(rowSize), _columnSize(columnSize), _elements(elements) {}
 
 #endif
