7a6f589fd0622822cc4762f134f47671173b113b
gckohaku <51472484+gckohaku@users.noreply.github.com>
2026-07-07 07:12:20 +0000

    edit test_utilities.hpp

--- tests/test_utilities.hpp
+++ tests/test_utilities.hpp
@@ -2,18 +2,21 @@
 #include <boost/test/included/unit_test.hpp>
 #include <cstddef>
 #include <cstdlib>
+#include <source_location>
+#include <sstream>
 template <typename T, size_t N>
 void CheckCloseEachArrayElement(
-    const std::array<T, N> left, const std::array<T, N> right, const std::array<T, N> tolerance
+    const std::array<T, N> left, const std::array<T, N> right, const std::array<T, N> tolerance,
+    const std::source_location& location = std::source_location::current()
 ) {
     for (size_t i = 0; i < N; i++) {
-        BOOST_CHECK_MESSAGE(
-            std::abs(left[i] - right[i]) <= tolerance[i],
-            "\nExceeds tolerance in index of " << i << ".\n"
-                                             << "Elements: (" << left[i] << ", " << right[i] << ")\n"
-                                             << "Expected error: " << tolerance[i] << "\n"
-                                             << "Actual Error" << std::abs(left[i] - right[i])
-                                             << "is exceeds expected error."
-        );
+        std::ostringstream message;
+        message << "\nFile: " << location.file_name() << " at " << location.line() << ":" << location.column() << "\n";
+        message << "Exceeds tolerance in index of " << i << ".\n";
+        message << "Elements: (" << left[i] << ", " << right[i] << "),\n";
+        message << "Expected error: " << tolerance[i] << ",\n";
+        message << "Actual Error: " << std::abs(left[i] - right[i]) << ".\n";
+        message << "Actual error exceeds expected error.";
+        BOOST_CHECK_MESSAGE(std::abs(left[i] - right[i]) <= tolerance[i], message.str());
     }
 }
